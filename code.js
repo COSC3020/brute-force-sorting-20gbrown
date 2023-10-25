dd09bce7c103f0ef4a193882b3969e70eb8bf445
gbrown20 <gbrown20@DESKTOP-2J0M7L5.localdomain>
2023-10-24 20:29:24 -0600

    Not working, will come back to it

--- code.js
+++ code.js
@@ -1,3 +1,30 @@
 function permutationSort(a) {
-    return 0;
+    function isSorted(arr) {
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function permute(arr, startIndex, count) {
+        if (startIndex === arr.length - 1) {
+            if (isSorted(arr)) {
+                return count + 1;
+            }
+            return count;
+        }
+
+        let minCount = count; //track the minimum count among all permutations
+        for (let i = startIndex; i < arr.length; i++) {
+            [arr[startIndex], arr[i]] = [arr[i], arr[startIndex]];
+            minCount = Math.min(minCount, permute(arr,startIndex + 1, count +1));
+            [arr[startIndex], arr[i]] = [arr[i], arr[startIndex]]; // Backtrack
+        }
+
+        return minCount;
+    }
+
+    return permute(a, 0, 0);
 }
