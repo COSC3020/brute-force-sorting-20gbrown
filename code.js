12219093eb57b5f35a4a96ff3d8de1b2c3f80d04
gbrown20 <gbrown20@DESKTOP-2J0M7L5.localdomain>
2023-11-07 16:07:08 -0700

    Fixed?

--- code.js
+++ code.js
@@ -1,35 +1,4 @@
-/*function permutationSort(a) {
-    function isSorted(arr) {
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i] < arr[i - 1]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    function permute(arr, startIndex, count) {
-        if (startIndex === arr.length - 1) {
-            if (isSorted(arr)) {
-                return count + 1;
-            }
-            return count;
-        }
-
-        let minCount = count; //track the minimum count among all permutations
-        for (let i = startIndex; i < arr.length; i++) {
-            [arr[startIndex], arr[i]] = [arr[i], arr[startIndex]];
-            minCount = Math.min(minCount, permute(arr,startIndex + 1, count +1));
-            [arr[startIndex], arr[i]] = [arr[i], arr[startIndex]]; // Backtrack
-        }
-
-        return minCount;
-    }
-
-    return permute(a, 0, 0);
-}*/
-
-//Not sure if this is technically a brute force sort, I can't get the above one to work 
+//Used pieces of both possibilities I had, should iterate over all permutations
 function permutationSort(a) {
     function isSorted(arr) {
         for (let i = 1; i < arr.length; i++) {
@@ -40,27 +9,28 @@ function permutationSort(a) {
         return true;
     }
 
-    function selectionSort(arr) {
-        let count = 0;
-
-        for (let i = 0; i < arr.length; i++) {
-            let minIndex = i;
-
-            for (let j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
+    function generatePermutations(arr, currentIdx) {
+        if (currentIdx === arr.length) {
+            if (isSorted(arr)) {
+                return true;
             }
-
-            if (minIndex !== i) {
-                [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-                count++;
-            }
+            return false;
         }
 
-        return count;
+        for (let i = currentIdx; i < arr.length; i++) {
+            [arr[i], arr[currentIdx]] = [arr[currentIdx], arr[i]]; 
+            if (generatePermutations(arr, currentIdx + 1)) {
+                return true;
+            }
+            [arr[i], arr[currentIdx]] = [arr[currentIdx], arr[i]]; 
+        }
+        return false;
     }
 
-    return selectionSort(a);
+    generatePermutations(a, 0);
+
+    return a;
 }
+
+
 
